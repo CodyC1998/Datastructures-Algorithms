03c068f7c21e13323faad0fc525ff16920781687
CodyC1998 <cody.collins@keyin.com>
2025-05-11 15:26:24 -0230

    average temp calculator working with user inputs

--- AvgTempAssignment/src/Main.java
+++ AvgTempAssignment/src/Main.java
@@ -1,3 +1,24 @@
+import java.util.Scanner;
+
 public class Main {
-    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("How many temperatures would you like to enter? ");
+        int numDays = scanner.nextInt();
+        double[] temperatures = new double[numDays];
+
+        for (int i = 0; i < numDays; i++) {
+            System.out.print("Day " + (i + 1) + "'s temperature: ");
+            temperatures[i] = scanner.nextDouble();
+        }
+
+        double average = TempCalc.calculateAverage(temperatures);
+        int countAboveAverage = TempCalc.countAboveAverage(temperatures, average);
+
+        System.out.printf("Average temperature: %.2f\n", average);
+        System.out.println(countAboveAverage + " day(s) above average");
+
+        scanner.close();
+    }
 }
